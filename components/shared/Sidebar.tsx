2807857e42b82eef811b02ff095f58496d2d945b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-02-29 11:21:41 -0500

    map sidebar links with styling in sidebar

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,7 +1,12 @@
+"use client"
 import Link from "next/link";
 import Image from "next/image";
+import { SignedIn } from "@clerk/nextjs";
+import { navLinks } from "@/constants";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname()
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
@@ -13,6 +18,20 @@ const Sidebar = () => {
             height={28}
           />
         </Link>
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = link.route === pathname
+                return (
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'} `}>
+                    {link.label}
+                  </li>
+                )
+              })}
+            </ul>
+          </SignedIn>
+        </nav>
       </div>
     </aside>
   );
