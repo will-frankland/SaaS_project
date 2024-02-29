d8bcde9d29503be89f5e201983aa2aab3664b5ba
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-02-29 11:23:50 -0500

    restyle sidebar links

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { SignedIn } from "@clerk/nextjs";
@@ -6,7 +6,7 @@ import { navLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
-  const pathname = usePathname()
+  const pathname = usePathname();
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
@@ -22,12 +22,28 @@ const Sidebar = () => {
           <SignedIn>
             <ul className="sidebar-nav_elements">
               {navLinks.map((link) => {
-                const isActive = link.route === pathname
+                const isActive = link.route === pathname;
                 return (
-                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'} `}>
-                    {link.label}
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    } `}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
                   </li>
-                )
+                );
               })}
             </ul>
           </SignedIn>
