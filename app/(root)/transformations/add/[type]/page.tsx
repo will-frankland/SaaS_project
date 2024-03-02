608d2dc0c4e7f06794419e757d757fb197f0e6a6
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-03-01 19:30:43 -0500

    create transformationForm component and import to trans add page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,10 +1,14 @@
 import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
 import { transformationTypes } from "@/constants";
 
 const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
   const transformation = transformationTypes[type];
   return (
+    <>
     <Header title={transformation.title} subtitle={transformation.subTitle} />
+    <TransformationForm />
+    </>
   );
 };
 
---
+++ components/shared/TransformationForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationForm = () => {
+  return (
+    <div>
+      Yes
+    </div>
+  )
+}
+
+export default TransformationForm
