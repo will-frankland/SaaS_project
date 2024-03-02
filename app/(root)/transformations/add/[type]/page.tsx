397e70366fce0bf451862198b185569106a119a6
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-03-01 19:27:55 -0500

    import transformation types and pass to header as props dynamically

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,11 @@
-import Header from '@/components/shared/Header'
+import Header from "@/components/shared/Header";
+import { transformationTypes } from "@/constants";
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
   return (
-    <Header title="Transformation Title" subtitle="Transformation Subtitle"/>
-  )
-}
+    <Header title={transformation.title} subtitle={transformation.subTitle} />
+  );
+};
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
