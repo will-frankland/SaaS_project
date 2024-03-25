7cde3932d5f0879023c02058cdb3377146615eea
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-03-25 14:12:46 -0400

    being building out Media Uploader and defining props

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,12 +1,61 @@
 import { useToast } from "../ui/use-toast"
+import { CldUploadWidget } from "next-cloudinary";
 
-const MediaUploader = () => {
+type MediaUploaderProps = {
+  onValueChange: (value: string) => void;
+  setImage: React.Dispatch<any>;
+  publicId: string;
+  image: any;
+  type: string;
+}
+
+const MediaUploader = ({ onValueChange, setImage, image, publicId, type }: MediaUploaderProps) => {
   const { toast } = useToast();
+
+  const onUploadSuccessHandler = (result: any) => {
+
+    toast({
+      title: "Image uploaded successfully",
+      description: "1 credit was deducted from your account",
+      duration: 5000,
+      className: 'success-toast'
+    })
+  }
+
+  const onUploadErrorHandler = () => {
+    toast({
+      title: "Something went wrong whilst uploading",
+      description: "Please try again",
+      duration: 5000,
+      className: 'error-toast'
+    })
+  }
+
   return (
-    <div>
-      
-    </div>
+    <CldUploadWidget
+      uploadPreset="jsm_PixelWhiz"
+      options={{
+        multiple: false,
+        resourceType: "image"
+      }}
+      onSuccess={onUploadSuccessHandler}
+      onError={onUploadErrorHandler}
+    >
+      {({ open }) => (
+        <div className="flex flex-col gap-4">
+          <h3 className="h3-bold text-dark-600">Original</h3>
+          {publicId ? (
+            <>
+            HERE IS IMAGE</>
+          ) : (
+            <div>
+              No IMAGE
+            </div>
+          )}
+        </div>
+      )}
+    </CldUploadWidget>
   )
 }
 
-export default MediaUploader
+export default MediaUploader;
