247fddde4721816d02b4a88cbde9b3299ef25216
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-03-25 14:00:38 -0400

    create uploadMedia file

---
+++ components/shared/MediaUploader.tsx
@@ -0,0 +1,12 @@
+import { useToast } from "../ui/use-toast"
+
+const MediaUploader = () => {
+  const { toast } = useToast();
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MediaUploader
