0aef4846ca3a8ee04b72178a787d9aed779998d5
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-03-25 10:25:48 -0400

    changes

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -5,22 +5,26 @@ import { getUserById } from "@/lib/actions/user.actions";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
   const { userId } = auth();
   const transformation = transformationTypes[type];
 
-  if(!userId) redirect('/sign-in');
+  if (!userId) redirect("/sign-in");
 
   const user = await getUserById(userId);
   return (
     <>
-    <Header title={transformation.title} subtitle={transformation.subTitle} />
-    <TransformationForm
-      action="Add"
-      userId={user._id}
-      type={transformation.type as TransformationTypeKey}
-      creditBalance={user.creditBalance}
-    />
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
   );
 };
