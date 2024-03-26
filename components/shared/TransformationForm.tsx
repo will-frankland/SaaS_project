6426b755c71fe306b138f40d40e84a0c2fd19928
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2024-03-26 10:19:54 -0400

    update useRouter import to solve error

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -35,7 +35,7 @@ import TransformedImage from "./TransformedImage";
 import { updateCredits } from "@/lib/actions/user.actions";
 import { getCldImageUrl } from "next-cloudinary";
 import { addImage, updateImage } from "@/lib/actions/image.actions";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export const formSchema = z.object({
   title: z.string(),
